f3c087c036410c4addf67e47229df777cbb98cc9
rawkintrevo <trevor.d.grant@gmail.com>
2025-04-03 11:42:07 -0500

    give in to ~~your hatred~~ the vibes
    Signed-off-by: rawkintrevo <trevor.d.grant@gmail.com>

--- tests/unit/testing/test_mcp.py
+++ tests/unit/testing/test_mcp.py
@@ -1,19 +1,16 @@
 import pytest
 import json
-import asyncio
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 @pytest.mark.asyncio
 async def invoke(cmd_or_url: str, method: str, data: dict) -> dict:
-    # STDIO transport
     command, args = cmd_or_url.split(" ", 1)
     server_params = StdioServerParameters(
         command=command,
         args=args.split(" "),
     )
     client = stdio_client(server_params)
-
     params = data if data else {}
 
     async with client as (read, write):
@@ -23,32 +20,26 @@ async def invoke(cmd_or_url: str, method: str, data: dict) -> dict:
             match method:
                 case "prompts/list":
                     result = await session.list_prompts()
-
                 case "prompts/get":
                     result = await session.get_prompt(**params)
-
                 case "resources/list":
                     result = await session.list_resources()
-
                 case "resources/read":
                     result = await session.read_resource(**params)
-
                 case "tools/list":
                     result = await session.list_tools()
-
                 case "tools/call":
                     result = await session.call_tool(**params)
-
                 case _:
                     raise ValueError(f"Unknown method: {method}")
-
             return result
+
 @pytest.mark.asyncio
 async def test_tool_list():
     cmd = "mcp run gofannon_server.py"
     method = "tools/list"
     data = {}
-    result = asyncio.run(invoke(cmd, method, data))
+    result = await invoke(cmd, method, data)
     assert any(t.name == "addition" for t in result['tools'])
 
 @pytest.mark.asyncio
@@ -56,5 +47,5 @@ async def test_fn():
     cmd = "mcp run gofannon_server.py"
     method = "tools/call"
     data = {"name": "addition", "arguments": {"num1": 1, "num2": 2}}
-    result = asyncio.run(invoke(cmd, method, data))
+    result = await invoke(cmd, method, data)
     assert result['content']['text'] == "3"
